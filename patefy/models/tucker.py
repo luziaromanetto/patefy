8a45305abf71549dbec7e5aacae17972aec74e40
luzia <luziaromanetto@gmail.com>
2016-08-08 18:11:36 -0300

    Adding file model/tucker.py that is an abstract class for tucker decomposition classes

---
+++ patefy/models/tucker.py
@@ -0,0 +1,43 @@
+import patefy.utils.multlinalg as MLA
+import numpy as np
+
+class TKD(object):
+	def __init__(self, T, facts):
+		self.T = T
+		self.C = None
+		self.B = None
+		self.R = facts
+		self.I = T.shape
+		self.N = len(T.shape)
+		self.err = None
+		
+	def error(self):
+		if self.err is None:
+			self.err = MLA.norm(self.T - MLA.tucker_operator( self.C, self.B ));
+			self.err = self.err/MLA.norm(self.T)
+			
+		return self.err
+
+	def pivotComponents(self, pivOrder, reverse=False):
+
+		if( reverse == True ):
+			pivOrder = [ p.reverse() for p in pivOrder ]
+
+		for i in range(len(pivOrder)):
+			Bi = np.array( self.B[i] , dtype='float64')
+			Bpi = np.array( self.B[i] , dtype='float64')
+
+			for j in range(len(pivOrder[i])):
+				for k in range(len(self.B[i])):
+					Bpi[k][j] = Bi[k][pivOrder[i][j]] 
+
+			self.B[i]=Bpi
+
+			Cp = np.zeros( self.R, dtype='float64')
+			for idAct in np.ndindex(tuple(self.R)):
+				idActOrd = tuple([ pivOrder[i][idAct[i]] for i in range(len(pivOrder)) ])
+
+				Cp[idAct]=self.C[idActOrd]
+				
+		self.C = Cp;
+
